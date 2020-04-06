39f8c7cf4f876dc2743273fb2e379d4d7bc4b384
brunoFpassaglia <bruno.passaglia2013@gmail.com>
2020-04-06 15:28:57 -0300

    Subject: implement counter of answers, add style for wrong and right answers.
    Problem:
    
    Solution:
    
    Note:

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,6 +1,16 @@
 <template>
   <b-nav tabs="">
     <b-nav-item active disabled="">Fancy quiz app</b-nav-item>
-    <b-nav-item disabled="">Counter:</b-nav-item>
+    <b-nav-item disabled="">Counter:{{numCorrect}}/{{numTotal}}</b-nav-item>
   </b-nav>
 </template>
+
+
+<script>
+export default {
+  props: [
+    'numCorrect',
+    'numTotal'  
+  ]
+}
+</script>
--- src/components/QuestionBox.vue
+++ src/components/QuestionBox.vue
@@ -14,27 +14,39 @@
         {{answere}}
       </p>-->
       <b-list-group>
-        <b-list-group-item @click="selectAnswer(index)" :class="[selectedIndex === index ? 'selected' : '']" v-for="(answere, index) in shuffledAnswers" :key="index">{{answere}}</b-list-group-item>
+        <b-list-group-item
+          @click="selectAnswer(index)"
+          :class="answereClass(index)"
+          v-for="(answere, index) in shuffledAnswers"
+          :key="index"
+        >{{answere}}</b-list-group-item>
       </b-list-group>
 
-      <b-button variant="primary" @click="submitAnswere">Subtmit</b-button>
+      <b-button
+        variant="primary"
+        @click="submitAnswer"
+        :disabled="selectedIndex===null || answered"
+      >Subtmit</b-button>
       <b-button variant="success" @click="next">Next</b-button>
     </b-jumbotron>
   </div>
 </template>
 
 <script>
-import _ from 'lodash'
+import _ from "lodash";
 export default {
   props: {
     currentQuestion: Object,
-    next: Function
+    next: Function,
+    increment: Function
   },
-  data(){
-    return  {
+  data() {
+    return {
       selectedIndex: null,
-      shuffledAnswers: []
-    }
+      shuffledAnswers: [],
+      correctIndex: null,
+      answered: false
+    };
   },
   computed: {
     answers() {
@@ -47,48 +59,70 @@ export default {
   watch: {
     currentQuestion() {
       this.selectedIndex = null;
+      this.answered = false;
       this.shuffleAnswers();
-
     }
   },
   methods: {
-    selectAnswer(index){
+    selectAnswer(index) {
       this.selectedIndex = index;
     },
-    shuffleAnswers(){
-      let answers = [...this.currentQuestion.incorrect_answers, this.currentQuestion.correct_answer];
+    shuffleAnswers() {
+      let answers = [
+        ...this.currentQuestion.incorrect_answers,
+        this.currentQuestion.correct_answer
+      ];
       this.shuffledAnswers = _.shuffle(answers);
+      this.correctIndex = this.shuffledAnswers.indexOf(
+        this.currentQuestion.correct_answer
+      );
     },
-    submitAnswere(){
+    submitAnswer() {
       let isCorrect = false;
-      if(this.selectedIndex === this.correctIndex){
+      if (this.selectedIndex === this.correctIndex) {
         isCorrect = true;
       }
-      return isCorrect;
+      this.answered = true;
+      this.increment(isCorrect);
+    },
+    answereClass(index) {
+      let answerClass = "";
+      if (!this.answered && this.selectedIndex === index) {
+        answerClass = "selected";
+      }
+
+      if (this.answered && this.correctIndex === index) {
+        answerClass = "correct";
+      }
+
+      if (this.answered && this.selectedIndex === index && this.correctIndex !== index) {
+        answerClass = "incorrect";
+      }
+      return answerClass;
     }
   },
   mounted() {
-    this.shuffleAnswers()
+    this.shuffleAnswers();
   }
 };
 </script>
 
 <style scoped>
-  .list-group-item {
-    margin-bottom: 15px;
-  }
-  .list-group-item:hover{
-    background-color: silver;
-    cursor: pointer;
-  }
-  .correct  {
-    background-color: greenyellow;
-  }
-  .incorrect  {
-    background-color: red;
-  }
-  .selected {
-    background-color: skyblue;
-  }
+.list-group-item {
+  margin-bottom: 15px;
+}
+.list-group-item:hover {
+  background-color: silver;
+  cursor: pointer;
+}
+.correct {
+  background-color: green;
+}
+.incorrect {
+  background-color: rgb(264, 0, 60);
+}
+.selected {
+  background-color: skyblue;
+}
 </style>
 
